9267e014c0fb3b48c0900c180e1421c5dd0af96c
Amogh Rao <amogh.r.rao@gmail.com>
2022-04-24 13:08:30 +0530

    Update index.js

--- src/pages/DashboardPage/index.js
+++ src/pages/DashboardPage/index.js
@@ -3,6 +3,7 @@ import Dashboard from '../../components/Dashboard'
 import './dashboard.css'
 
 const DashboardPage = () => {
+  document.title = 'Dashboard | IPL Fantasy League';
   return (
     <div className='tablecontainer'><Dashboard /></div>
   )
