9956b7cc65fd49d28584740d69c9c05486194b37
Amogh Rao <amogh.r.rao@gmail.com>
2022-04-24 17:23:14 +0530

    Indentation Fix

--- src/components/AdminAddButton/TeamsForm.js
+++ src/components/AdminAddButton/TeamsForm.js
@@ -5,19 +5,19 @@ const TeamsForm = () => {
     <div>
         <div class="popup">
             <div class="close-btn">&times;</div>
-            <div class="form">
-            <div class= "form-element">
-                <label for="email">Email</label>
-                <input type="text" id="email" placeholder="Enter email" />
+                <div class="form">
+                <div class= "form-element">
+                    <label for="email">Email</label>
+                    <input type="text" id="email" placeholder="Enter email" />
+                    </div>
+                <div class="form-element">
+                    <label for ="password">Password</label>
+                    <input type="password" id="password" placeholder="Enter password" />
                 </div>
-            <div class="form-element">
-                <label for ="password">Password</label>
-                <input type="password" id="password" placeholder="Enter password" />
+                <div class="form-element">
+                    <button>Sign in</button>
+                </div>
             </div>
-        <div class="form-element">
-            <button>Sign in</button>
-        </div>
-        </div>
         </div>
     </div>
   )
